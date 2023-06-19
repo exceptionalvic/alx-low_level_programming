c127e4c106deacbc38bc2a9422df219cce968808
Victor Igbokwe <igbokwev@gmail.com>
2023-06-19 23:03:20 +0000

    added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of short: %zu byte(s)\n", sizeof(short));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of long: %zu byte(s)\n", sizeof(long));
+	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	
+	return 0;
+}
